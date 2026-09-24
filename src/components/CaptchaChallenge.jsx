e36e86b8f46e9fb91eb163f1e5cc44132eee5a50
Daniel <Daniel@gmail.com>
2026-09-24 15:04:31 -0400

    fix unused varible fix

--- src/components/CaptchaChallenge.jsx
+++ src/components/CaptchaChallenge.jsx
@@ -91,10 +91,6 @@ function getStepMatchers(osType) {
   return [isWinR, isCtrlV, isEnterKey];
 }
 
-function usesBlurForFirstStep(osType) {
-  return osType === "Windows";
-}
-
 function detectOS() {
   if (typeof navigator === "undefined") {
     return "Unknown";
@@ -280,10 +276,6 @@ function CaptchaChallenge({ onVerified }) {
     },
     [osType]
   );
-
-  const advanceStep = useCallback(() => {
-    advanceBySteps(1);
-  }, [advanceBySteps]);
 
   const positionVerifyWindow = useCallback(() => {
     const checkboxWindow = checkboxWindowRef.current;
